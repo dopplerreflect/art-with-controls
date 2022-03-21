ce67b7caf17523acda7ddd1ae3df2adc1f58f011
David Rose <doppler@gmail.com>
2022-03-21 16:34:19 -0500

    dynamically render controls

--- src/gallery/One.tsx
+++ src/gallery/One.tsx
@@ -1,27 +1,32 @@
 import { ChangeEvent, useReducer, useRef, useState } from 'react';
 import '../Image.css';
 
-type Controls = {
-  hue: number;
-  radius: number;
+type Control = {
+  name: string;
+  value: number;
+  min: number;
+  max: number;
 };
+
+type Controls = Control[];
 
 type ReducerAction = {
   control: string;
   value: number;
 };
 
-const controlsInitialState: Controls = {
-  hue: 180,
-  radius: 30,
-};
+const controlsInitialState: Controls = [
+  { name: 'hue', value: 180, min: 0, max: 359 },
+  { name: 'radius', value: 45, min: 1, max: 64 },
+];
 
 const reducer = (controls: Controls, action: ReducerAction) => {
   const { control, value } = action;
-  switch (control) {
-    default:
-      return { ...controls, [control]: value };
-  }
+  const newControls: Controls = [
+    ...controls.filter(c => c.name !== control),
+    { ...controls.find(c => c.name === control)!, value },
+  ].sort((a, b) => (a.name > b.name ? 1 : -1));
+  return newControls;
 };
 
 const One = () => {
@@ -41,27 +46,26 @@ const One = () => {
     <div className='Container'>
       <div ref={controlsRef} className='Controls hidden'>
         CONTROLS
-        <input
-          name='hue'
-          type='range'
-          min={0}
-          max={359}
-          value={controls.hue}
-          onChange={handleControlChange}
-        />
-        <input
-          name='radius'
-          type='range'
-          min={1}
-          max={64}
-          value={controls.radius}
-          onChange={handleControlChange}
-        />
+        {controls.map(control => (
+          <input
+            key={control.name}
+            type='range'
+            name={control.name}
+            min={control.min}
+            max={control.max}
+            onChange={handleControlChange}
+            value={controls.find((c: Control) => c.name === control.name)!.value}
+          />
+        ))}
       </div>
       <div className='Image' onClick={toggleControls}>
         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 128 128'>
-          <rect width={128} height={128} fill={`hsl(${controls.hue}, 100%, 50%)`} />
-          <circle cx={64} cy={64} r={controls.radius} />
+          <rect
+            width={128}
+            height={128}
+            fill={`hsl(${controls.find(c => c.name === 'hue')!.value}, 100%, 50%)`}
+          />
+          <circle cx={64} cy={64} r={controls.find(c => c.name === 'radius')!.value} />
         </svg>
       </div>
     </div>
