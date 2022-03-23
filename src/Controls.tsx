46bec0874a359a9444f67d4270e27dcebf0ba7df
David Rose <doppler@gmail.com>
2022-03-22 21:35:24 -0500

    preserve order of Controls arrays

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -37,10 +37,11 @@ const Controls = ({ controls, handleControlChange }: ControlsProps) => {
 
 const reducer = (controls: Controls, action: ReducerAction) => {
   const { control, value } = action;
-  const newControls: Controls = [
-    ...controls.filter(c => c.name !== control),
-    { ...controls.find(c => c.name === control)!, value },
-  ].sort((a, b) => (a.name > b.name ? 1 : -1));
+  // done this way to preserve order
+  const controlToReplace = controls.find(c => c.name === control)!;
+  const indexOfControlToReplace = controls.indexOf(controlToReplace);
+  const newControls = [...controls];
+  newControls[indexOfControlToReplace] = { ...controlToReplace, value };
   return newControls;
 };
 
