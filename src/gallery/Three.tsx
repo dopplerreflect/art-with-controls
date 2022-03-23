ba5a1c13f73cd700401ca1fe91463f0d241335cf
David Rose <doppler@gmail.com>
2022-03-23 09:56:28 -0500

    fix css to work with both Gallery and standalone Container

--- src/gallery/Three.tsx
+++
@@ -1,8 +0,0 @@
-const Three = () => {
-  return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 128 128'>
-      <rect width={128} height={128} fill='green' />
-    </svg>
-  );
-};
-export default Three;
